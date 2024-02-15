58e85a241d5a19dd5569ae4c9526106261deef57
Mayank <mayank0real0world@gmail.com>
2024-02-15 18:26:23 +0530

    add : middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -39,7 +39,7 @@ export default function ProfilePage() {
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >Logout</button>
 
-        <button
+        <button 
         onClick={getUserDetails}
         className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >GetUser Details</button>
