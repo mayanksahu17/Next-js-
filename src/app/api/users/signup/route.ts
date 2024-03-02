3e08d733e1c7ca450bc0fea97411499d5ad8a873
Mayank <mayank0real0world@gmail.com>
2024-03-02 21:59:23 +0530

    Feture : mail shooting for verification

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,7 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-// import { sendEmail } from "@/helpers/mailer";
+import { sendEmail } from "@/helpers/mailer";
 
 
 connect()
@@ -37,7 +37,7 @@ export async function POST(request: NextRequest){
 
         //send verification email
 
-        // await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
             message: "User created successfully",
